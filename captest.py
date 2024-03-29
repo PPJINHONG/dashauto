0e9f97d323fc425edceb7a12ee673bf54b351eb6
ppjinhong <pearljh19@gmail.com>
2024-03-29 16:36:40 +0900

    capture03291529

--- captest.py
+++ captest.py
@@ -8,10 +8,9 @@ from selenium import webdriver
 from concurrent.futures import ThreadPoolExecutor
 import threading
 
-# 이미지 폴더 지정
+
 screenshots_path = 'ScreenShots'
 
-# 이미지 폴더 존재 유무 체크 (필요 시 생성)
 isExist = os.path.exists(screenshots_path)
 if not isExist:
     os.makedirs(screenshots_path)
@@ -35,14 +34,14 @@ class Driver:
     def __del__(self):
         self.driver.quit() # clean up driver when we are cleaned up
 
-# 스레드별로 구분되는 네임스페이스 제공
+
 thread_local = threading.local()
 
 # 웹페이지 로그인 제공
-def login(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath):
+def login2(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath):
     # Grafana 로그인 페이지로 이동
     driver.get(url)
-    sleep(100)
+    sleep(30)
     driver.switch_to.default_content()
     driver.switch_to.parent_frame()
 
@@ -50,7 +49,7 @@ def login(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath):
         driver.implicitly_wait(3)        
         # 고급 버튼 클릭
         driver.find_element('xpath', '//*[@id="details-button"]').click()
-        sleep(10)
+        sleep(5)
 
         # 이동 버튼 클릭
         driver.find_element('xpath', '//*[@id="proceed-link"]').click()
@@ -58,7 +57,7 @@ def login(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath):
 
         #Hcloud 추가
         driver.find_element('xpath', '//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div[2]/div[2]/a').click() 
-        sleep(10)
+        sleep(5)
 
     except Exception as e:
         print(e)
@@ -85,132 +84,211 @@ def login(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath):
 
         sleep(3)
 
-# 웹드라이버 클래스와 로그인 함수를 사용한, 스레드별 웹페이지 띄우기
-# 결과: 로그인 된 화면
+def login(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath):
+    try:
+        # Grafana 로그인 페이지로 이동
+        driver.get(url)
+        driver.implicitly_wait(30)  
+        driver.switch_to.default_content()
+        driver.switch_to.parent_frame()
+
+        try:
+                  
+            # 고급 버튼 클릭
+            driver.find_element('xpath', '//*[@id="details-button"]').click()
+            driver.implicitly_wait(10)  
+
+            # 이동 버튼 클릭
+            driver.find_element('xpath', '//*[@id="proceed-link"]').click()
+            driver.implicitly_wait(10)  
+
+            # Hcloud 추가
+            driver.find_element('xpath', '//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div[2]/div[2]/a').click() 
+            driver.implicitly_wait(10)  
+
+        except Exception as e:
+            print(e)
+
+        
+        driver.implicitly_wait(3)           
+    
+        username = driver.find_element('xpath', userid_xpath)    
+        print("id입력 성공")                                        
+        username.clear()
+        username.send_keys(userid)
+        sleep(3)
+
+        
+        password = driver.find_element('xpath', passwd_xpath)
+        print("pw입력 성공") 
+        password.clear()
+        password.send_keys(passwd)
+        sleep(3)
+
+        
+        # Login 버튼 클릭
+        driver.find_element('xpath', login_xpath).click()
+        print("login시도중---->>"+url)
+        sleep(3)
+
+    except Exception as e:
+        print(f"Error occurred during login: {e}")
+        # 에러 메시지 출력 후 종료
+        sys.exit(1)
+
+
+# 로그인 정보 딕셔너리
+login_info = {
+    "k8s_cluster_1center_ccskr-rancher-prd": {
+        "url": "https://hubble-apne2-prd.platform.hcloud.io/grafana/login/generic_oauth",
+        "userid": "cocop",
+        "passwd": "cocop",
+        "userid_xpath": '//*[@id="username"]',
+        "passwd_xpath": '//*[@id="password"]',
+        "login_xpath": '//*[@id="kc-form-login"]/button'
+    },
+    "k8s_cluster_1center_ccskr-devworks-prd": {
+        "url": "http://10.11.67.29:3000/login",
+        "userid": "readonly",
+        "passwd": "readonly!23",
+        "userid_xpath": '//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div/form/div[1]/div[2]/div/div/input',
+        "passwd_xpath": '//*[@id="current-password"]',
+        "login_xpath": '//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div/form/button'
+    },
+    "k8s_cluster_1center_ccskr-dkc2hpkr-prd": {
+        "url": "http://10.11.67.29:3000/login",
+        "userid": "readonly",
+        "passwd": "readonly!23",
+        "userid_xpath": '//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div/form/div[1]/div[2]/div/div/input',
+        "passwd_xpath": '//*[@id="current-password"]',
+        "login_xpath": '//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div/form/button'
+    }
+}
+
 def create_driver(bot):
     the_driver = getattr(thread_local, 'the_driver', None)
+    
     if the_driver is None:
-
-        # 웹드라이버 생성
         try:
             the_driver = Driver()
             setattr(thread_local, 'the_driver', the_driver)
+
         except Exception as e:
             print(e)
-            pass
+            return None
 
-        # Special Initialization to login:
-        try:
-            driver = the_driver.driver
-        except Exception as e:
-            print(e)
-            pass
+    driver = the_driver.driver
 
-        print("Step 1, create_driver")
-        print(bot)
-        sleep(30)
-        if bot == "k8s_cluster_1center_ccskr-rancher-prd":
-            # Login Info
-            url = "https://hubble-apne2-prd.platform.hcloud.io/grafana/login/generic_oauth"
-            userid = 'cocop'
-            passwd = 'cocop'
-            userid_xpath='//*[@id="username"]'
-            passwd_xpath='//*[@id="password"]'
-            login_xpath='//*[@id="kc-form-login"]/button'
-            # Login                
-            login(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath)
-        elif bot == "k8s_cluster_1center_ccskr-devworks-prd":
-            # Login Info
-            url = "http://10.11.67.29:3000/login"
-            userid = 'readonly'
-            passwd = 'readonly!23'
-            userid_xpath='//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div/form/div[1]/div[2]/div/div/input'
-            passwd_xpath='//*[@id="current-password"]'
-            login_xpath='//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div/form/button'  
-            # Login                
-            login(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath)
+    print("create_driver"+bot)
+    print(bot)
+    sleep(30)
 
-        elif bot == "k8s_cluster_1center_ccskr-dkc2hpkr-prd":
-            # Login Info
-            url = "http://10.11.67.29:3000/login"
-            userid = 'readonly'
-            passwd = 'readonly!23'
-            userid_xpath='//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div/form/div[1]/div[2]/div/div/input'
-            passwd_xpath='//*[@id="current-password"]'
-            login_xpath='//*[@id="reactRoot"]/div/main/div[3]/div/div[2]/div/div/form/button'  
-            # Login                
+    try:
+        login_data = login_info.get(bot)
+        if login_data:
+            url = login_data["url"]
+            userid = login_data["userid"]
+            passwd = login_data["passwd"]
+            userid_xpath = login_data["userid_xpath"]
+            passwd_xpath = login_data["passwd_xpath"]
+            login_xpath = login_data["login_xpath"]
             login(driver, url, userid, userid_xpath, passwd, passwd_xpath, login_xpath)
-       
-      
+    except Exception as e:
+        print(e)
+
     return driver
 
-def take_screenshot(bot):
-    print("Step 1, take_screenshot")
+
+
+def take_screenshot1(bot):
+    print("--take_screenshot--")
     # 웹드라이버 생성
     try:
         driver = create_driver(bot)
-    except KeyboardInterrupt:
-        print('Caught keyboardinterrupt')
-        pass
+        if driver is None:
+            return  # 드라이버 생성 실패 시 함수 종료
 
-    print("Step 2, take_screenshot")   
-
-    try:
-        while True:
-            try:
-                print(f"Capturing the screens started at {datetime.now()}")
-                print(f"It captures every 5 mins")
-                print("Step 3, take_screenshot")
-
-                start_time = time.time()
-
-                # 최종(대시보드) 페이지 및 저장파일 이름 설정
-                if bot == "k8s_cluster_1center_ccskr-rancher-prd":
-                    url = 'https://hubble-apne2-prd.platform.hcloud.io/grafana/d/coBoVkG4z/kr-apne1-rancher-local?orgId=27'
-                    filename = screenshots_path + '/KR1_Rancher-prd_' + datetime.now().strftime('%Y%m%d_%H%M') + '.png'
-                elif bot == "k8s_cluster_1center_ccskr-devworks-prd":
-                    url = 'http://10.11.67.29:3000/d/1HihRd54k/coc-k8s-summary-dashboard-alert-system-kr1-kr_devworks_prd_cluster?orgId=1'
-                    filename = screenshots_path + '/KR1_ccskr-devworks-prd_' + datetime.now().strftime('%Y%m%d_%H%M') + '.png'
-                elif bot == "k8s_cluster_1center_ccskr-dkc2hpkr-prd":
-                    url = 'http://10.11.67.29:3000/d/oOYkgdc4z/coc-k8s-summary-dashboard-alert-system-kr1-dkc2-hpkr?orgId=1'
-                    filename = screenshots_path + '/KR1_ccskr-dkc2hpkr-prd_' + datetime.now().strftime('%Y%m%d_%H%M') + '.png'
-                try:
-                    driver.get(url)                 
-                    sleep(180)                 
-                    driver.set_window_size(1920,1080)
-                    sleep(10)
-                    driver.maximize_window()
-                    sleep(10)
-                    driver.execute_script("document.body.style.zoom=0.80")
-                    sleep(2)
-                except Exception as e:
-                    print(e)
-
-                # 화면 캡처 수행
-                try:
-                    driver.save_screenshot(filename)
-                except KeyboardInterrupt:
-                    print('Caught keyboardinterrupt')
-                    pass                
-                        
-                print("Step 4, take_screenshot")
-
-            # 예외 처리
-            except (KeyboardInterrupt, SystemExit):
-                print("\nkeyboardinterrupt caught")
-                print("\n... Program Stopped Manually!")
-                exiting.set()
-            break
-
+        print(f"Capturing the screens started at {datetime.now()}")
+        
+        start_time = time.time()
+        
+        if bot == "k8s_cluster_1center_ccskr-rancher-prd":
+            url = 'https://hubble-apne2-prd.platform.hcloud.io/grafana/d/coBoVkG4z/kr-apne1-rancher-local?orgId=27'
+            filename = f'{screenshots_path}/KR1_Rancher-prd_{datetime.now().strftime("%Y%m%d_%H%M")}.png'
+        elif bot == "k8s_cluster_1center_ccskr-devworks-prd":
+            url = 'http://10.11.67.29:3000/d/1HihRd54k/coc-k8s-summary-dashboard-alert-system-kr1-kr_devworks_prd_cluster?orgId=1'
+            filename = f'{screenshots_path}/KR1_ccskr-devworks-prd_{datetime.now().strftime("%Y%m%d_%H%M")}.png'
+        elif bot == "k8s_cluster_1center_ccskr-dkc2hpkr-prd":
+            url = 'http://10.11.67.29:3000/d/oOYkgdc4z/coc-k8s-summary-dashboard-alert-system-kr1-dkc2-hpkr?orgId=1'
+            filename = f'{screenshots_path}/KR1_ccskr-dkc2hpkr-prd_{datetime.now().strftime("%Y%m%d_%H%M")}.png'
+        try:
+            driver.get(url)                 
+            sleep(180)                 
+            driver.set_window_size(1920, 1080)
+            sleep(10)
+            driver.maximize_window()
+            sleep(10)
+            driver.execute_script("document.body.style.zoom=0.80")
+            sleep(2)
+        except Exception as e:
+            print(e)
+        # 화면 캡처 수행
+        
+        try:
+            driver.save_screenshot(filename)
+        except KeyboardInterrupt:
+            print('Caught keyboardinterrupt')
+            pass                
+                    
+        print("Step 4, take_screenshot")
     # 예외 처리
     except (KeyboardInterrupt, SystemExit):
         print("\nkeyboardinterrupt caught")
         print("\n... Program Stopped Manually!")
         exiting.set()
 
+def take_screenshot(bot):
+    print("--take_screenshot--")
+    # 웹드라이버 생성
+    try:
+        driver = create_driver(bot)
+        if driver is None:
+            return  # 드라이버 생성 실패 시 함수 종료
+
+        print(f"Capturing the screens started at {datetime.now()}")
+        print("Step 3, take_screenshot")
+        start_time = time.time()
+        # 최종(대시보드) 페이지 및 저장파일 이름 설정
+        urls = {
+            "k8s_cluster_1center_ccskr-rancher-prd": 'https://hubble-apne2-prd.platform.hcloud.io/grafana/d/coBoVkG4z/kr-apne1-rancher-local?orgId=27',
+            "k8s_cluster_1center_ccskr-devworks-prd": 'http://10.11.67.29:3000/d/1HihRd54k/coc-k8s-summary-dashboard-alert-system-kr1-kr_devworks_prd_cluster?orgId=1',
+            "k8s_cluster_1center_ccskr-dkc2hpkr-prd": 'http://10.11.67.29:3000/d/oOYkgdc4z/coc-k8s-summary-dashboard-alert-system-kr1-dkc2-hpkr?orgId=1'
+        }
+        filename = f'{screenshots_path}/{bot}_{datetime.now().strftime("%Y%m%d_%H%M")}.png'
+        url = urls.get(bot)
+        if url:
+            driver.get(url)
+            driver.implicitly_wait(30)  
+            driver.set_window_size(1920, 1080)
+            driver.implicitly_wait(10)  
+            driver.maximize_window()
+            driver.implicitly_wait(10)  
+            driver.execute_script("document.body.style.zoom=0.80")
+            driver.implicitly_wait(3)  
+            driver.save_screenshot(filename)
+        else:
+            print("Invalid bot specified.")
+        print("Step 4, take_screenshot")
+    # 예외 처리
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        print("\n... Program Stopped Manually!")
+        return
+
+
 def process_screencapture():
 
-    number_threads = 11
+    number_threads = 3
     
 
     bots = [
@@ -234,8 +312,6 @@ if __name__ == "__main__":
         print('Caught keyboardinterrupt')
         pass
 
-    # 스레드 네임스페이스 삭제
-    # Quit the selenium drivers:
     del thread_local
 
     # 메모리 캐시 삭제
